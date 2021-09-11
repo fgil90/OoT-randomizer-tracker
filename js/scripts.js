7ffba281251a7b9f14c409db911397cd17bd1a9c
Felipe Gil <fgil90@gmail.com>
2021-09-11 18:16:34 -0300

    Add WOTH and FOOL tracking.

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,8 @@ const totalGridItems = gridSizeX * gridSizeY
 const grid = document.querySelector(".grid")
 const wothWrapper = document.querySelector(".wrapper-woth")
 const foolWrapper = document.querySelector(".wrapper-fool")
-const woth = document.querySelector("#woth")
-const fool = document.querySelector("#fool")
+const wothInput = document.querySelector("#woth")
+const foolInput = document.querySelector("#fool")
 
 const strengths = ["strength1", "strength2", "strength3"]
 const ocarinas = ["ocarina1", "ocarina2"]
@@ -32,29 +32,33 @@ const gridImageArray =
         [0, 0, 0, 0, 0, 0, 0]
     ]
 
+// Loop though all the grid items
+
 for (let i = 0; i < totalGridItems; i++) {
 
-    let xPosition = i % 6
-    let yPosition = i / 6 >> 0
+    const xGrid = i % 6
+    const yGrid = i / 6 >> 0
 
-    let gridItem = document.createElement("div")
+    const gridItem = document.createElement("div")
     // gridItem.dataset.x = xPosition
     // gridItem.dataset.y = yPosition
 
-    if (!(gridImageArray[yPosition][xPosition] == "skulltula" ||
-        gridImageArray[yPosition][xPosition] == "kokiri_tunic" ||
-        gridImageArray[yPosition][xPosition] == "boots")) {
+    if (!(gridImageArray[yGrid][xGrid] == "skulltula" ||
+        gridImageArray[yGrid][xGrid] == "kokiri_tunic" ||
+        gridImageArray[yGrid][xGrid] == "boots")) {
         gridItem.classList.add('grey')
     }
 
-    if (gridImageArray[yPosition][xPosition] instanceof Array) {
+    //Is the grid item an upgradeable? If yes ...
 
-        let tempArray = gridImageArray[yPosition][xPosition]
+    if (gridImageArray[yGrid][xGrid] instanceof Array) {
+
+        const tempArray = gridImageArray[yGrid][xGrid]
 
         gridItem.style.backgroundImage = `url(src/${tempArray[0]}.png)`
 
         gridItem.addEventListener('click', function () {
-            if (this.classList.contains("grey")) {
+            if (this.classList.contains('grey')) {
                 this.classList.remove('grey')
                 return
             }
@@ -78,7 +82,7 @@ for (let i = 0; i < totalGridItems; i++) {
                         e.target.classList.add('grey')
                         return
                     }
-                    
+
                     e.target.classList.remove('grey')
                     e.target.style.backgroundImage = `url(src/${tempArray[(i + tempArray.length - 1) % tempArray.length]}.png`;
                     return
@@ -90,27 +94,29 @@ for (let i = 0; i < totalGridItems; i++) {
         continue
     }
 
-    gridItem.style.backgroundImage = `url(src/${gridImageArray[yPosition][xPosition]}.png)`
+    gridItem.style.backgroundImage = `url(src/${gridImageArray[yGrid][xGrid]}.png)`
 
-    if (gridImageArray[yPosition][xPosition] == "skulltula") {
+    if (gridImageArray[yGrid][xGrid] == "skulltula") {
         const skullCount = document.createElement("p")
+        let skulltulaCounter = 0
+
         skullCount.classList.add("skull-count")
-        skullCount.textContent = 0
+        skullCount.textContent = skulltulaCounter
         gridItem.appendChild(skullCount)
         gridItem.addEventListener('click', function () {
-            if (parseInt(skullCount.textContent) >= 100) {
-                skullCount.textContent = 100
-                return
+            skulltulaCounter += 1
+            if (skulltulaCounter >= 100) {
+                skulltulaCounter = 100
             }
-            skullCount.textContent = parseInt(skullCount.textContent) + 1;
+            skullCount.textContent = skulltulaCounter;
         })
         gridItem.addEventListener('contextmenu', function (e) {
             e.preventDefault()
-            if (parseInt(skullCount.textContent) <= 0) {
-                skullCount.textContent = 0
-                return
+            skulltulaCounter -= 1
+            if (skulltulaCounter <= 0) {
+                skulltulaCounter = 0
             }
-            skullCount.textContent = parseInt(skullCount.textContent) - 1;
+            skullCount.textContent = skulltulaCounter;
         })
         grid.appendChild(gridItem)
         continue
@@ -121,9 +127,40 @@ for (let i = 0; i < totalGridItems; i++) {
     })
     gridItem.addEventListener('contextmenu', (e) => {
         e.preventDefault()
-        e.target.classList.toggle('grey')
+        e.target.classList.add('grey')
     })
-
 
     grid.appendChild(gridItem)
 }
+
+//Finish looping though all grid items
+//All grid items are initialized
+
+function createTrackedLocation(trackedLocation, anchor) {
+    const div = document.createElement('div')
+    anchor.appendChild(div)
+
+    const label = document.createElement("label")
+    const checkbox = document.createElement("input")
+
+    checkbox.type = "checkbox"
+    checkbox.classList.add("tracked-location")
+
+    label.textContent = trackedLocation
+
+    div.appendChild(label)
+    div.appendChild(checkbox)
+}
+
+wothInput.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+        createTrackedLocation(this.value, wothWrapper)
+        this.value = ""
+    }
+})
+foolInput.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+        createTrackedLocation(this.value, foolWrapper)
+        this.value = ""
+    }
+})
