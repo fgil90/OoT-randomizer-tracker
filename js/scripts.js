f6db1344336284fb9daf150dc5fee2dbc490c431
Felipe Gil <fgil90@gmail.com>
2021-09-11 23:03:30 -0300

    Add dungeon medallion tracking
    Change biggoron sword to trade sequence.

--- js/scripts.js
+++ js/scripts.js
@@ -14,22 +14,25 @@ const foolInput = document.querySelector("#fool")
 const strengths = ["strength1", "strength2", "strength3"]
 const ocarinas = ["ocarina1", "ocarina2"]
 const masks = ["zeldas_letter", "keaton_mask", "skull_mask", "spooky_mask", "bunny_hood", "mask_of_truth"]
+const adultTrade = ["egg", "chicken", "cojiro", "mushroom", "medicine", "poachers_saw", "broken_sword", "prescription", "eyeball_frog", "eyedrops", "biggoron_sword"]
 const scales = ["scale1", "scale2"]
 const wallets = ["wallet1", "wallet2"]
 const hookshots = ["hookshot", "longshot"]
+
+const dungeons = ["DEKU", "DDCV", "JABU", "FRST", "FIRE", "WATR", "SPRT", "SHDW", "FREE"]
 
 const gridImageArray =
     [
         ["deku_stick", "slingshot", "boomerang", "deku_nut", ocarinas, "lens_of_truth"],
         ["bombs", "bombchus", "magic_beans", "bow", hookshots, "megaton_hammer"],
-        ["fire_arrows", "ice_arrows", "light_arrows", "kokiri_sword", "master_sword", "biggoron_sword"],
+        ["fire_arrows", "ice_arrows", "light_arrows", "kokiri_sword", "master_sword", adultTrade],
         ["dins_fire", "farores_wind", "nayrus_love", "deku_shield", "hylian_shield", "mirror_shield"],
         ["rutos_letter", masks, "skulltula", "kokiri_tunic", "goron_tunic", "zora_tunic"],
         [scales, wallets, strengths, "boots", "iron_boots", "hover_boots"],
         ["zeldas_lullaby", "sarias_song", "eponas_song", "suns_song", "song_of_time", "song_of_storms"],
         ["minuet_of_forest", "bolero_of_fire", "serenade_of_water", "requiem_of_spirit", "nocturne_of_shadow", "prelude_of_light"],
         ["light_medallion", "forest_medallion", "fire_medallion", "water_medallion", "spirit_medallion", "shadow_medallion"],
-        [0, 0, 0, 0, 0, 0, 0]
+        [dungeons, dungeons, dungeons, dungeons, dungeons, dungeons]
     ]
 
 // Loop though all the grid items
@@ -49,11 +52,39 @@ for (let i = 0; i < totalGridItems; i++) {
         gridItem.classList.add('grey')
     }
 
-    //Is the grid item an upgradeable? If yes ...
+    //Is the grid item an array? If yes ...
 
     if (gridImageArray[yGrid][xGrid] instanceof Array) {
 
         const tempArray = gridImageArray[yGrid][xGrid]
+
+        //If it is the dungeons array
+
+        if (tempArray[0] == 'DEKU') {
+            const dungeonNameContainer = document.createElement('p')
+            dungeonNameContainer.textContent = tempArray[0]
+            gridItem.appendChild(dungeonNameContainer)
+
+            gridItem.addEventListener('click', function () {
+                currentIndex = tempArray.indexOf(dungeonNameContainer.textContent)
+                for (let i = currentIndex; i < tempArray.length; i++) {
+                    dungeonNameContainer.textContent = tempArray[(i + 1) % tempArray.length]
+                    return
+                }
+            })
+            gridItem.addEventListener('contextmenu', (e) => {
+                e.preventDefault();
+                currentIndex = tempArray.indexOf(dungeonNameContainer.textContent)
+                for (let i = currentIndex; i < tempArray.length; i++) {
+                    dungeonNameContainer.textContent = tempArray[(i - 1 + tempArray.length) % tempArray.length]
+                    return
+                }
+            })
+            grid.appendChild(gridItem)
+            continue
+        }
+
+        // if it is an upgrade
 
         gridItem.style.backgroundImage = `url(src/${tempArray[0]}.png)`
 
